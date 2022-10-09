74fa4ba2b66385e88d16d23f1365d7a323f3f9c9
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-09 12:22:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-c17-l13
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fh4dsg)
---
+++ src/router.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App';
+import Homepage from './pages/Homepage/Homepage';
+import Profile from './pages/Profile/Profile';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+import ProfileOverview from './pages/Profile/pages/ProfileOverview/ProfileOverview';
+import ProfileData from './pages/Profile/pages/ProfileData/ProfileData';
+import ProfileForm from './pages/Profile/pages/ProfileForm/ProfileForm';
+import { rootLoader } from './loaders/rootLoader';
+import { profileLoader } from './loaders/profileLoader';
+import { profileFormAction } from './actions/profileFormAction';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    loader: rootLoader,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: '/profile',
+        element: <Profile />,
+        loader: profileLoader,
+        caseSensitive: true,
+        children: [
+          {
+            index: true,
+            element: <ProfileOverview />,
+          },
+          {
+            path: 'data',
+            element: <ProfileData />,
+          },
+          {
+            path: 'form',
+            action: profileFormAction,
+            element: <ProfileForm />,
+          },
+        ],
+      },
+    ],
+  },
+]);
